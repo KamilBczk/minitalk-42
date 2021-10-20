3681fff947a5bbef9f096bd689ff198d2756477a
Kamil Biczyk <kamil.biczyk.pro@gmail.com>
2021-10-20 22:48:43 +0200

    h file

---
+++ src/includes/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/18 14:40:50 by kamilbiczyk       #+#    #+#             */
+/*   Updated: 2021/10/18 14:57:25 by kamilbiczyk      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#endif
