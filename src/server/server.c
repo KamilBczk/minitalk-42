e4e402e4c4020f9a1955818e13d66fe0bb7800c1
Kamil <kamil.biczyk.pro@gmail.com>
2022-03-08 16:10:22 +0100

    c file (server.c)

--- src/server/server.c
+++ src/server/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kamilbiczyk <kamilbiczyk@student.42.fr>    +#+  +:+       +#+        */
+/*   By: kbiczyk <kbiczyk@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 14:29:39 by kamilbiczyk       #+#    #+#             */
-/*   Updated: 2021/10/20 22:40:29 by kamilbiczyk      ###   ########.fr       */
+/*   Updated: 2022/03/08 16:07:42 by kbiczyk          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,23 @@
 
 void handler(int num)
 {   
-    if (num == 30)
-        write(1, "0", 1);
-    else if (num == 31)
-        write(1, "1", 1);
+    printf("dss");
+    if(num == 30)
+        printf("0");
     else
-        printf("Error");
+        printf("1");
 }
 
 int main(void)
 {
-    int pid;
-    struct sigaction sa;
+    pid_t pid;
 
     pid = getpid();
-    sa.sa_handler = handler;
-    printf("Pid : %d\n", pid);
+    printf("Server Pid : %d\n", pid);
     signal(SIGUSR1, handler);
     signal(SIGUSR2, handler);
-    // sigaction(SIGUSR1, &sa, NULL);
-    // sigaction(SIGUSR2, &sa, NULL);
     while (1)
     {
-        
+        sleep(1);
     }
 }
